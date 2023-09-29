95fdbf36d912422cb88d8a18408c72ae7c38d1b9
Dmitry Antonov <AntonovDmitry21@gmail.com>
2023-09-29 21:17:21 +0300

    Supabase BackEnd

---
+++ src/services/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://jirfkvddclwojpwymbnw.supabase.co'
+const supabaseKey =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImppcmZrdmRkY2x3b2pwd3ltYm53Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTYwMDcyMTcsImV4cCI6MjAxMTU4MzIxN30.CNTgudek4BDDDY3vqVZWMvOsjWhydLPdwlGScJTcA74'
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+export default supabase
