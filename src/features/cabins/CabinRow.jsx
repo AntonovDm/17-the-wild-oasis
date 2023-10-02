d5088b5f0d39d382760e3fcf3d7657d6d238ae90
Dmitry Antonov <AntonovDmitry21@gmail.com>
2023-10-02 13:22:39 +0300

    Upd name (editCabin => updateCabin etc.)

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -102,7 +102,7 @@ function CabinRow({ cabin }) {
           </button>
         </div>
       </TableRow>
-      {showForm && <CreateCabinForm cabinToEdit={cabin} />}
+      {showForm && <CreateCabinForm cabinToUpdate={cabin} />}
     </>
   )
 }
--- src/features/cabins/CreateCabinForm.jsx
+++ src/features/cabins/CreateCabinForm.jsx
@@ -8,27 +8,27 @@ import Textarea from '../../ui/Textarea'
 import FormRow from '../../ui/FormRow'
 
 import { useCreateCabin } from './useCreateCabin'
-import { useEditCabin } from './useEditCabin'
+import { useUpdateCabin } from './useUpdateCabin'
 
-function CreateCabinForm({ cabinToEdit = {}, onCloseModal }) {
+function CreateCabinForm({ cabinToUpdate = {}, onCloseModal }) {
   const { isCreating, createCabin } = useCreateCabin()
-  const { isEditing, editCabin } = useEditCabin()
-  const isWorking = isCreating || isEditing
+  const { isUpdating, updateCabin } = useUpdateCabin()
+  const isWorking = isCreating || isUpdating
 
-  const { id: editId, ...editValues } = cabinToEdit
-  const isEditSession = Boolean(editId)
+  const { id: updateId, ...updateValues } = cabinToUpdate
+  const isUpdateSession = Boolean(updateId)
 
   const { register, handleSubmit, reset, getValues, formState } = useForm({
-    defaultValues: isEditSession ? editValues : {},
+    defaultValues: isUpdateSession ? updateValues : {},
   })
   const { errors } = formState
 
   function onSubmit(data) {
     const image = typeof data.image === 'string' ? data.image : data.image[0]
 
-    if (isEditSession)
-      editCabin(
-        { newCabinData: { ...data, image }, id: editId },
+    if (isUpdateSession)
+      updateCabin(
+        { newCabinData: { ...data, image }, id: updateId },
         {
           onSuccsess: (data) => {
             reset()
@@ -145,7 +145,7 @@ function CreateCabinForm({ cabinToEdit = {}, onCloseModal }) {
           id="image"
           accept="image/*"
           {...register('image', {
-            required: isEditSession ? false : 'This field is required',
+            required: isUpdateSession ? false : 'This field is required',
           })}
         />
       </FormRow>
@@ -160,7 +160,7 @@ function CreateCabinForm({ cabinToEdit = {}, onCloseModal }) {
           Cancel
         </Button>
         <Button disabled={isWorking}>
-          {isEditSession ? 'Edit cabin' : 'Create new cabin'}
+          {isUpdateSession ? 'Edit cabin' : 'Create new cabin'}
         </Button>
       </FormRow>
     </Form>
